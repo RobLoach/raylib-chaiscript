48b8c21e57947c7b166f9241a948137da536832c
Rob Loach <robloach@gmail.com>
2019-01-27 17:37:02 -0500

    Update raylib-chaiscript binary

--- bin/raylib-chaiscript.cpp
+++ bin/raylib-chaiscript.cpp
@@ -43,27 +43,54 @@
 *
 ********************************************************************************************/
 
+#include <iostream>
+#include <string>
+
+#include "raylib.h"
 #include "chaiscript/extras/raylib.hpp"
 
 int main(int argc, char *argv[])
 {
-    if (argc > 1)
-    {
-        if (IsFileExtension(argv[1], ".chai"))
-        {
-            // Create the ChaiScript environment.
-            chaiscript::ChaiScript chai;
+    std::string executableName;
+    std::string fileToLoad;
 
-            // Load the ChaiScript module.
-            auto raylib = chaiscript::extras::raylib::bootstrap();
-
-            // Add the library to the ChaiScript instance.
-            chai.add(raylib);
-
-            // Execute the script.
-            chai.eval_file(argv[1]);
-        }
+    switch (argc) {
+        case 0:
+            executableName = "raylib-chaiscript";
+            fileToLoad = "main.chai";
+            break;
+        case 1:
+            executableName = argv[0];
+            fileToLoad = "main.chai";
+            break;
+        default:
+            executableName = argv[0];
+            fileToLoad = argv[1];
+            break;
     }
+
+    if (!FileExists(fileToLoad.c_str())) {
+        std::cout << "Usage:" << std::endl << "    " << GetFileName(executableName.c_str()) << " myfile.chai" << std::endl << std::endl;
+        std::cout << "Attempted file " << fileToLoad << " was not found." << std::endl;
+        return 1;
+    }
+
+    if (!IsFileExtension(fileToLoad.c_str(), ".chai")) {
+        std::cout << "Expected file to be a .chai file." << std::endl;
+        return 1;
+    }
+
+    // Create the ChaiScript environment.
+    chaiscript::ChaiScript chai;
+
+    // Load the ChaiScript module.
+    auto raylib = chaiscript::extras::raylib::bootstrap();
+
+    // Add the library to the ChaiScript instance.
+    chai.add(raylib);
+
+    // Execute the script.
+    chai.eval_file(fileToLoad);
 
     return 0;
 }
